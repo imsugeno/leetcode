ce2ee751cc03fe9596593fc8653fb8d20d496de5
imsugeno <g.tokyo.kazusa@gmail.com>
2026-04-17 00:04:54 +0900

    refactor

--- 49_group_anagrams.go
+++ 49_group_anagrams.go
@@ -1,19 +1,19 @@
 package leetcode
 
 func groupAnagrams(strs []string) [][]string {
-	counterToList := make(map[[26]int][]string)
+	groups := make(map[[26]int][]string)
 
-	for _, str := range(strs) {
-		var counter [26]int
-		for _, s := range(str) {
-			counter[s-'a']++
+	for _, str := range strs {
+		var key [26]int
+		for _, c := range str {
+			key[c-'a']++
 		}
-		counterToList[counter] = append(counterToList[counter], str)
+		groups[key] = append(groups[key], str)
 	}
 
-	var result [][]string
-	for _, l := range(counterToList) {
-		result = append(result, l)
+	result := make([][]string, 0, len(groups))
+	for _, group := range groups {
+		result = append(result, group)
 	}
 
 	return result
