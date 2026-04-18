3c20431ed86e4676be69c7023a9436245cd6464f
imsugeno <g.tokyo.kazusa@gmail.com>
2026-04-18 17:46:01 +0900

    backtrack

---
+++ 78_subsets_another.go
@@ -0,0 +1,22 @@
+package leetcode
+
+func subsets2(nums []int) [][]int {
+	var result [][]int
+	var current []int
+
+	var backtrack func(start int)
+	backtrack = func(start int) {
+		tmp := make([]int, len(current))
+		copy(tmp, current)
+		result = append(result, tmp)
+
+		for i := start; i < len(nums); i++ {
+			current = append(current, nums[i])
+			backtrack(i + 1)
+			current = current[:len(current)-1]
+		}
+	}
+
+	backtrack(0)
+	return result
+}
