8fa9442b47d27912761eebf1c5f70b0d3351a422
imsugeno <g.tokyo.kazusa@gmail.com>
2026-04-18 15:11:50 +0900

    自力

--- 1886_determine.go
+++ 1886_determine.go
@@ -1,19 +1,35 @@
 package leetcode
 
 func findRotation(mat [][]int, target [][]int) bool {
-	for i := 0; i < 4; i++ {
-		if equal(mat, target) {
-			return true
-		}
-		mat = rotate(mat)
-	}
-	return false
+    for range 4 {
+        if equal(mat, target) {
+            return true
+        }
+        mat = rotate(mat)
+    }
+    return false
 }
 
-func equal(a, b [][]int) bool {
-	for i := range a {
-		for j := range a[i] {
-			if a[i][j] != b[i][j] {
+func rotate(mat[][]int) [][]int {
+	n := len(mat)
+	result := make([][]int, n)
+	for i, v := range(mat) {
+		result[i] = make([]int, len(v))
+	}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			result[j][n-1-i] = mat[i][j]
+		}
+	}
+
+	return result
+}
+
+func equal(a [][]int, b [][]int) bool {
+	for i, row := range(a) {
+		for j, v := range(row) {
+			if v != b[i][j] {
 				return false
 			}
 		}
@@ -21,18 +37,20 @@ func equal(a, b [][]int) bool {
 	return true
 }
 
-// rotate returns mat rotated 90 degrees clockwise
-// mat[i][j] -> result[j][n-1-i]
-func rotate(mat [][]int) [][]int {
-	n := len(mat)
-	res := make([][]int, n)
-	for i := range res {
-		res[i] = make([]int, n)
-	}
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			res[j][n-1-i] = mat[i][j]
-		}
-	}
-	return res
-}
+/***
+a, b, c
+d, e, f
+g, h, i
+
+g, d, a
+h, e, b
+i, f, c
+
+j=0列目　-> 0行目
+j=1列目　-> 1行目
+j=2列目　-> 2行目
+
+i=0行目 -> 2列目 n(3) - 1 - j(0) = 2
+i=1行目 -> 1列目 n(3) - 1 - j(1) = 1
+i=2行目 -> 0列目 n(3) - 1 - j(2) = 0
+***/
