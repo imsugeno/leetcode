dbb6073e1a931b38a7e0cc1fe3cc4407da52cc92
imsugeno <g.tokyo.kazusa@gmail.com>
2026-04-18 16:49:36 +0900

    自力

--- 78_subsets.go
+++ 78_subsets.go
@@ -1,23 +1,27 @@
 package leetcode
 
 func subsets(nums []int) [][]int {
-	result := [][]int{}
-	current := []int{}
+	var result [][]int
+	var current []int
 
 	var dfs func(i int)
 	dfs = func(i int) {
+		// 葉に到達したらリターン
 		if i == len(nums) {
 			tmp := make([]int, len(current))
 			copy(tmp, current)
 			result = append(result, tmp)
 			return
 		}
+
 		// 入れない
 		dfs(i + 1)
 
 		// 入れる
 		current = append(current, nums[i])
 		dfs(i + 1)
+
+		// 戻す
 		current = current[:len(current)-1]
 	}
 
