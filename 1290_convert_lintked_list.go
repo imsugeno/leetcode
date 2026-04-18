e1c6ec378034c1b39a85a2bde56c0c53f9ef60fb
imsugeno <g.tokyo.kazusa@gmail.com>
2026-04-18 20:28:57 +0900

    天才

--- 1290_convert_lintked_list.go
+++ 1290_convert_lintked_list.go
@@ -1,27 +1,14 @@
 package leetcode
 
-import (
-	"strconv"
-)
-
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
 func getDecimalValue(head *ListNode) int {
-	var binary string
-	bs := getBinary(head, binary)
-	result, _ := strconv.ParseInt(bs, 2, 64)
-
-	return int(result)
-}
-
-func getBinary(head *ListNode, binary string) string {
-	binary += strconv.Itoa(head.Val)
-	if head.Next == nil {
-		return binary
+	result := 0
+	for node := head; node != nil; node = node.Next {
+		result = result<<1 | node.Val
 	}
-
-	return getBinary(head.Next, binary)
+	return result
 }
