27c6a89d723da864bafa308d38268b336962cea3
imsugeno <g.tokyo.kazusa@gmail.com>
2026-04-18 21:39:56 +0900

    ok

---
+++ 19_remove_nth_node.go
@@ -0,0 +1,21 @@
+package leetcode
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	totalNodes := 0
+	for node := head; node != nil; node = node.Next {
+		totalNodes ++
+	}
+	
+	count := 0
+	for node := head; node != nil; node = node.Next {
+		if totalNodes - n - 1 < 0 {
+			return head.Next
+		}
+		if count == totalNodes - n - 1 {
+			node.Next = node.Next.Next
+		}
+		count ++
+	}
+
+	return head
+}
