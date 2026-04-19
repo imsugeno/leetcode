18bc75d26b7028138dd30ccb8705bb5e1ced8ce3
imsugeno <g.tokyo.kazusa@gmail.com>
2026-04-19 23:59:30 +0900

    add

---
+++ 744_find_smallest.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func nextGreatestLetter(letters []byte, target byte) byte {
+	lo, hi := 0, len(letters)
+	for lo < hi {
+		mid := lo + (hi-lo)/2
+		if letters[mid] > target {
+			hi = mid
+		} else {
+			lo = mid + 1
+		}
+	}
+	if lo == len(letters) {
+		return letters[0]
+	}
+	return letters[lo]
+}
