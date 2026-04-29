704718ffcf7e15d393aba586702d53be1ed02fcf
imsugeno <g.tokyo.kazusa@gmail.com>
2026-04-30 02:27:33 +0900

    miss

---
+++ at_vacation/at_vacation.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Buffer(make([]byte, 1<<20), 1<<20)
+	sc.Scan()
+	n, _ := strconv.Atoi(sc.Text())
+
+	points := make([][]int, n)
+	for i := 0; i <= n; i++ {
+		sc.Scan()
+		pointStr := strings.Split(sc.Text(), " ")
+		buf := make([]int, 3)
+		for j, p := range pointStr {
+			buf[j], _ = strconv.Atoi(p)
+		}
+		points[i] = buf
+	}
+
+	// dp[i][j]: i-1日目までの幸福度の総和の最大値(0-indexed)
+	// j 0:a 1:b 2:c を選択
+	// j:0 -> i日目は 1or2
+	// j:1 -> i日目は 0or2
+	// j:2 -> i日目は 0or1
+	dp := make([][]int, n)
+	for i := range n {
+		dp[i] = []int{0, 0, 0}
+	}
+	for i := range n {
+		for j := range 3 {
+			for k := range 3 {
+				if j == k {
+					continue
+				}
+				chmax(&dp[i+1][k], dp[i][j]+points[i][k])
+			}
+		}
+	}
+
+	max := 0
+	for j := range 3 {
+		chmax(&max, dp[n][j])
+	}
+	fmt.Println(max)
+}
+
+func chmax(a *int, b int) bool {
+	if *a < b {
+		*a = b
+		return true
+	}
+	return false
+}
